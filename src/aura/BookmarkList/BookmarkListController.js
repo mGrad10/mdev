5e1457f9df340b6011a60b2d408798577e8f895c
Melinda <mgrad@sandiego.edu>
2019-02-24 23:48:55 -0800

    WIP displays bookmarks but error when creating favorite

--- src/aura/BookmarkList/BookmarkListController.js
+++ src/aura/BookmarkList/BookmarkListController.js
@@ -4,5 +4,13 @@
 ({
     doInit: function (component,event,helper) {
         helper.getBookmarkList(component,event,helper);
+    },
+
+    doCreateFavorite: function (component, event, helper) {
+
+        var bookmarkToFavorite = event.getSource().get('v.value');
+
+        helper.createFavorite(component,event,helper,bookmarkToFavorite);
+
     }
 })
--- src/aura/BookmarkList/BookmarkListHelper.js
+++ src/aura/BookmarkList/BookmarkListHelper.js
@@ -11,17 +11,16 @@
 
         action.setCallback(this,function(response) {
             var state = response.getState();
-            console.log('STATE ==');
             if (state === "SUCCESS") {
-                console.log("Response results: " + response.getReturnValue().bookmarks[0].bookmark.URL__c);
-                component.set('v.bookmarkList', response.getReturnValue());
+                console.log("Response results: ");
+                console.log(response.getReturnValue().bookmarks);
+                component.set('v.bookmarkList', response.getReturnValue().bookmarks);
             }
             else {
                 var errors = response.getError();
                 if (errors) {
                     if (errors[0] && errors[0].message) {
-                        console.log("Error message: " +
-                            errors[0].message);
+                        helper.showToast(component,event,helper,'Error', errors[0].message);
                     }
                 } else {
                     console.log("Unknown error");
@@ -29,5 +28,37 @@
             }
         });
         $A.enqueueAction(action);
+    },
+
+    createFavorite: function (component,event,helper,bookmarkToFavorite) {
+
+        console.log('INSIDE CREATE FAVS');
+
+        //Call Apex controller to get Bookmarks
+        var action = component.get("c.createUserFavoriteBookmark");
+
+        action.setParam('theBookmark', bookmarkToFavorite);
+
+        action.setCallback(this,function(response) {
+            if (response.getReturnValue().success) {
+                console.log('SUCCESS');
+                helper.showToast(component,event,helper,'Success', 'Favorites Updated');
+            }
+            else {
+
+                helper.showToast(component,event,helper,'Error', response.getReturnValue().exceptionMessages[0]);
+
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    showToast : function(component, event, helper, title, message) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": title,
+            "message": message
+        });
+        toastEvent.fire();
     }
 })
